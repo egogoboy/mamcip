1e31bea07ecc652c43ade5a06ba04ef65b792171
egogoboy <egor2005sudakov@gmail.com>
2026-03-27 10:36:45 +0300

    encoding: move key into methods' parameters

--- src/encoding.cpp
+++ src/encoding.cpp
@@ -4,27 +4,25 @@ namespace encoding {
 
 const QString alphabet = "_АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЭЮЯ";
 
-const int K = 5;
-
 size_t get_char_idx(QChar c) { return alphabet.indexOf(c); }
 
-QString encode(const QString& input_text) {
+QString encode(const QString& input_text, int key) {
     QString result;
     result.reserve(input_text.size());
 
     for (QChar c : input_text) {
-        result.push_back(alphabet.at(get_char_idx(c) + K % alphabet.size()));
+        result.push_back(alphabet.at(get_char_idx(c) + key % alphabet.size()));
     }
 
     return result;
 }
 
-QString decode(const QString& input_text) {
+QString decode(const QString& input_text, int key) {
     QString result;
     result.reserve(input_text.size());
 
     for (auto c : input_text) {
-        result.push_back(alphabet.at(get_char_idx(c) - K % alphabet.size()));
+        result.push_back(alphabet.at(get_char_idx(c) - key % alphabet.size()));
     }
 
     return result;
